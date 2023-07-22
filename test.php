59b7bacd8941bb6b18ee4fe1f1664b527edf543b
mohammadaminS <140054076+mohammadaminS@users.noreply.github.com>
2023-07-22 21:33:20 +0330

    اتمام پروژه

--- test.php
+++ test.php
@@ -1,24 +1,42 @@
-<html>
-
-<head>
-    <TItle>
-        تبیدل تاریخ
-    </TItle>
-</head>
-<link rel="stylesheet" href="test.php">
-
-<body style="background-color: aquamarine;">
-    <h1 style="text-align: center;">
-        تبدیل تاریخ میلادی به شمسی
-    </h1>
-    <form method="post">
-        <input value="Date" type="date">
-        <input type="submit">
-    </form>
-</body>
-
-</html>
 <?php
 $date = $_POST['Date'];
-echo $date;
+$chekD = 0;
+$chekM = 0;
+
+
+
+$y = substr($date,2,3);
+$m = substr($date,5,7);
+$d = substr($date,8,9);
+$m = substr($m,0,2);
+
+
+$d = substr($d,1,2);
+$m = substr($m,1,2);
+$y = substr($y,0,2);
+
+// echo $y.'*'.$m.'*'.$d;
+
+if ($d <= 21){
+$chekD = 1;
+$d = 31 + ($d - 21);
+echo $d.'*';
+}
+
+if ($chekD == 1){
+    $m-=1;
+}
+
+if ($m <= 2){
+    $chekM = 1;
+    $m = 12 + ($m - 2);
+}
+if ($chekM == 1){
+    $y -= 1;
+}
+$y -= 21;
+
+echo $y.'**'.$m.'**'.$d;
+
+
 ?>
